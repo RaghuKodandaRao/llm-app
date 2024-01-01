518212c9ac11f17ad503a0d1f682dd8756cb45ea
RaghuKodandaRao <109233481+RaghuKodandaRao@users.noreply.github.com>
2024-01-01 17:35:55 +0530

    Modified server.py to include & invoke Knowledge Graph

--- examples/ui/server.py
+++ examples/ui/server.py
@@ -3,6 +3,18 @@ import os
 import requests
 import streamlit as st
 from dotenv import load_dotenv
+from streamlit_modal import Modal
+
+import webbrowser as wb
+page_params = st.experimental_get_query_params()
+
+#-------->>>>>>>>
+#Author: Raghu Kodanda - Imported sys to include knowledgeGraph
+## knowledgeGraph.showKwGraph enables to Display the response from LLM App as a Knowedge Grap using graphviz 
+import sys
+import graphviz
+from knowledgeGraph import showKwGraph
+#<<<<<<<<<<--------
 
 with st.sidebar:
     st.markdown(
@@ -22,6 +34,57 @@ st.title("LLM App")
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
+
+#-------->>>>>>>>
+#Author: Raghu Kodanda - Imported sys to include knowledgeGraph
+## 
+##Maintaining Click status of button to hide graph upon click
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = False
+
+if st.session_state.clicked:
+    st.session_state.clicked = False
+#<<<<<<<<<<--------
+
+
+#-------->>>>>>>>
+#Author: Raghu Kodanda - Imported sys to include knowledgeGraph
+## 
+##showGraph Helper method to prepare & invoke knowledgeGraph.showKwGraph
+def showGraph(gpText):
+    print("Showing Grpah Here ...")
+    #showKwGraph(gpText)
+
+    if page_params:
+        text = page_params.get("text")[0]
+        st.write(text)
+        print("Page Params")
+    else:
+        #text = st.text_input("Enter Text")
+        text = "KnowledgeGraph"
+        btn = st.button("Hide the Graph")
+        #st.graphviz_chart(showGraph(response))
+        #showGraph(response)
+        showKwGraph(response)
+        print("Page Parms else")
+        if btn:
+            print("Button if")
+            st.markdown(f"http://localhost:8501/?text={text}")
+            wb.open_new_tab(f"http://localhost:8501/?text={text}")
+#if page_params:
+#   text = page_params.get("text")[0]
+#   st.write(text)
+
+#else:
+   #text = st.text_input("Enter Text")
+#   text = "KnowledgeGraph"
+#   btn = st.button("Click to open")
+
+#   if btn:
+#       st.markdown(f"http://localhost:8501/?text={text}") 
+#       wb.open_new_tab(f"http://localhost:8501/?text={text}")
+#<<<<<<<<<<--------
+
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
@@ -48,6 +111,25 @@ if prompt := st.chat_input("How can I help you today?"):
         with st.chat_message("assistant"):
             st.markdown(response)
         st.session_state.messages.append({"role": "assistant", "content": response})
+        
+        showGraph(response)
+        
+        #if page_params:
+        #     text = page_params.get("text")[0]
+        #     st.write(text)
+        #     print("Page Params")
+        #else:
+             #text = st.text_input("Enter Text")
+        #     text = "KnowledgeGraph"
+        #     btn = st.button("Hide the Graph")
+             #st.graphviz_chart(showGraph(response))
+             #showGraph(response)
+        #     print("Page Parms else")
+        #     if btn:
+        #          print("Button if")
+        #          st.markdown(f"http://localhost:8501/?text={text}")
+        #          wb.open_new_tab(f"http://localhost:8501/?text={text}")
+
     else:
         st.error(
             f"Failed to send data to Discounts API. Status code: {response.status_code}"
